8b0b5b1903a06471c348d88915fb8a1ac40c1906
jooooel <joel.guedj@gmail.com>
2022-11-27 11:08:18 +0100

    First commit
    Basic setup with Spectre.Console.Cli and a simple command reading a path

---
+++ src/tye-vnext.cli/ConfigFileFinder.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace tye_vnext.cli
+{
+    public class ConfigFileFinder
+    {
+        private static readonly string[] FileFormats = { "*.csproj" };
+
+        public static bool TryFindSupportedFile(string directoryPath, [NotNullWhen(true)] out string? filePath, [MaybeNullWhen(true)] out string? errorMessage, string[]? fileFormats = null)
+        {
+            fileFormats ??= FileFormats;
+            foreach (var format in fileFormats)
+            {
+                var files = Directory.GetFiles(directoryPath, format);
+
+                switch (files.Length)
+                {
+                    case 1:
+                        errorMessage = null;
+                        filePath = files[0];
+                        return true;
+                    case 0:
+                        continue;
+                }
+
+                errorMessage = $"More than one matching file was found in directory '{directoryPath}'.";
+                filePath = default;
+                return false;
+            }
+
+            errorMessage = $"No project project file or solution was found in directory '{directoryPath}'.";
+            filePath = default;
+            return false;
+        }
+    }
+}
---
+++ src/tye-vnext.cli/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using Spectre.Console;
+using Spectre.Console.Cli;
+using tye_vnext.cli;
+
+var app = new CommandApp<RunCommand>();
+return app.Run(args);
+
+internal sealed class RunCommand : Command<RunCommand.Settings>
+{
+    public sealed class Settings : CommandSettings
+    {
+        [Description("Path to tye.yaml. Defaults to current directory.")]
+        [CommandArgument(0, "[path]")]
+        public string? Path { get; init; }
+    }
+
+    public override int Execute([NotNull] CommandContext context, [NotNull] Settings settings)
+    {
+        var fileInfo = TryParsePath(settings.Path);
+
+        if (fileInfo is null || !fileInfo.Exists)
+        {
+            AnsiConsole.MarkupLine($"[red]File not found {settings.Path}[/]");
+            return -1;
+        }
+
+        AnsiConsole.MarkupLine($"Running Tye from [green]{fileInfo.FullName}[/]");
+        return 0;
+    }
+
+    private static FileInfo? TryParsePath(string? path)
+    {
+        if (string.IsNullOrEmpty(path))
+        {
+            path = ".";
+        }
+
+        if (File.Exists(path))
+        {
+            return new FileInfo(path);
+        }
+
+        if (Directory.Exists(path))
+        {
+            if (ConfigFileFinder.TryFindSupportedFile(path, out var filePath, out var errorMessage))
+            {
+                return new FileInfo(filePath!);
+            }
+
+            return default;
+        }
+
+        return default;
+    }
+}
